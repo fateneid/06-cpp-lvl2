f5862c87f80f1ddb9a34e2727c2f598a819f2f6d
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 03:42:44 +0300

    remove unnecessary lines and comments

--- 06-introduction-to-programming-using-cpp-level-2.cpp
+++ 06-introduction-to-programming-using-cpp-level-2.cpp
@@ -1,51 +1,4 @@
 #include <iostream>
-#include "L02_BreakpointAndMemoryValues.h"
-#include "L08_CreateYourOwnLibrary.h"
-#include "L09_TernaryOperatorShortHandIF.h"
-#include "L10_RangedLoop.h"
-#include "L11_ValidateNumber.h"
-#include "L12_BitwiseANDoperator.h"
-#include "L13_BitwiseORoperator.h"
-#include "L14_DeclarationVsDefinition.h"
-#include "L15_DefaultParameters.h"
-#include "L16_FunctionOverloading.h"
-#include "L17_CallStackOrCallHierarchy.h"
-#include "L18_VSFunctionTips.h"
-#include "L19_Recursion.h"
-#include "L20_StaticVariables.h"
-#include "L21_AutomaticVariables.h"
-#include "L23_IntegerFormatPrintf.h"
-#include "L24_FloatFormatPrintf.h"
-#include "L25_StringandCharFormatPrintf.h"
-#include "L26_SetwManipulator.h"
-#include "L27_TwoDimensionalArrays.h"
-#include "L28_VectorIntroductionDeclarationandInitialization.h"
-#include "L29_VectorAddElements.h"
-#include "L30_VectorOfStructure.h"
-#include "L34_CreatingReferences.h"
-#include "L35_WhatIsPointer.h"
-#include "L36_DereferencingPointer.h"
-#include "L37_CommonMistakesWithPointers.h"
-#include "L39_CallByReferenceAndUsingPointers.h"
-#include "L40_PointersAndArrays.h"
-#include "L41_PointersAndStructure.h"
-#include "L42_PointerToVoid.h"
-#include "L43_MemoryManagementNewAndDelete.h"
-#include "L44_DynamicArraysNewAndDelete.h"
-#include "L46_AccessElementsInVector.h"
-#include "L47_ChangeElementsInVector.h"
-#include "L48_VectorIterators.h"
-#include "L49_TryCatch.h"
-#include "L50_StringObjectCommonMethods.h"
-#include "L51_SomeCCTYPEFunctions.h"
-#include "L52_WriteModeWriteDataToFile.h"
-#include "L53_AppendModeAppendDataToFile.h"
-#include "L54_ReadModePrintFileContent.h"
-#include "L55_LoadDataFromFileToVector.h"
-#include "L56_SaveVectorToFile.h"
-#include "L57_DeleteRecordFromFile.h"
-#include "L58_UpdateRecordInFile.h"
-#include "L59_DatetimeLocalUTCTime.h"
 #include "L60_DatetimeStructure.h"
 
 using namespace std;
@@ -53,20 +6,8 @@ using namespace std;
 int main()
 {
 
+
     L60_DatetimeStructure::Task();
 
 
 }
-
-
-/*
-
-library's namespace name = library name
-
-To run any problem:
-
-    - include its library
-    - use its namespace with any method
-    - run Task fun under its namespace
-
-*/
